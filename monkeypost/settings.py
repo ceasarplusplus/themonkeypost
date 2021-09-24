3795bd7550f120175edc9f00b26112bf857a1dd6
ceasar <ceasardoyin@gmail.com>
2021-09-24 12:44:25 +0100

    postgress

--- monkeypost/settings.py
+++ monkeypost/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'monkeypost.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'monkeypostdb',
+        'USER': 'ceasar',
+        'PASSWORD': '@1Monkey_king',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
