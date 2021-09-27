07466e70227e3d7927b028781f4b5fac0a42099c
ceasar <ceasardoyin@gmail.com>
2021-09-27 06:26:37 +0100

    static dir

--- monkeypost/settings.py
+++ monkeypost/settings.py
@@ -268,7 +268,7 @@ AWS_S3_CUSTOM_DOMAIN = config('AWS_S3_CUSTOM_DOMAIN')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-# AWS_LOCATION = 'static'
+AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
@@ -277,9 +277,9 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 # # STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, 'static')
+STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
 
-MEDIA_URL =  'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, 'media')
+MEDIA_URL =  f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
 
 STATICFILES_STORAGE =  'custom_storages.StaticStorage'
 
