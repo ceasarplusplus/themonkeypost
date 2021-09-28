b32971a4ab41e61cf1606552e76505c0dd38ebcc
ceasar <ceasardoyin@gmail.com>
2021-09-28 10:42:30 +0100

    new static settingsss

--- monkeypost/settings.py
+++ monkeypost/settings.py
@@ -216,8 +216,8 @@ ROBOTS_SITEMAP_URLS = [
 
 # =============== Dev settings ===========================================
 
-Static files (CSS, JavaScript, Images)
-https://docs.djangoproject.com/en/3.1/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
