6a309b119dfb446e3d807c8908a70daea2e94cd9
ceasar <ceasardoyin@gmail.com>
2021-09-28 13:27:58 +0100

    ckeditorrrr

--- monkeypost/settings.py
+++ monkeypost/settings.py
@@ -278,10 +278,10 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 # DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # # STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
